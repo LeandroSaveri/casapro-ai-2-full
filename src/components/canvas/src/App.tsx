1206d79ef5a000f9ba7ee12539b8ae1754047319
LeandroSaveri <leandrosaveri1106@gmail.com>
2026-02-24 15:54:42 -0300

    Create App.tsx

---
+++ src/components/canvas/src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import { Canvas2D } from './components/canvas/Canvas2D';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <Canvas2D />
+    </div>
+  );
+}
+
+export default App;
